4465dc1004109339d2ad57a48f9efe42439bae33
SwegCet <ethantruong0804@gmail.com>
2025-10-20 22:25:46 -0700

    Added login page, app shell so NavBar is consistent after logging in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,41 @@ import "./App.css";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import NavBar from "./components/navBar";
+import Login from "./pages/Login";
+import ProtectedRoutes from "./routes/ProtectedRoutes";
 import { MovieProvider } from "./contexts/MovieContext";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthProvider";
 
-function App() {
+function AppShell() {
+  // Shared layout for pages
   return (
-    <MovieProvider>
+    <>
       <NavBar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </main>
-    </MovieProvider>
+      <Outlet />
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <AuthProvider>
+      <MovieProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected app shell*/}
+          <Route element={<ProtectedRoutes />}>
+            <Route element={<AppShell />}>
+              <Route index element={<Home />} />
+              <Route path="favorites" element={<Favorites />} />
+            </Route>
+          </Route>
+
+          {/*Fallback*/}
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </MovieProvider>
+    </AuthProvider>
+  );
+}
