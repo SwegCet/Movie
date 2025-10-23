6939d3d8a2da42ccfb3917223c3f16e2c1521586
SwegCet <ethantruong0804@gmail.com>
2025-10-23 00:03:35 -0700

    Added signup component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,11 +3,12 @@ import { useNavigate, Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthProvider";
 
 export default function Login() {
-  const { user, login } = useAuth();
+  const { user, login, signup } = useAuth();
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setErr] = useState("");
+  const [isSignup, setisSignup] = useState(false); // Alternates between login/signup
 
   if (user) {
     return <Navigate to="/" replace />; // Navigates user to home page
@@ -17,17 +18,22 @@ export default function Login() {
     e.preventDefault();
     setErr("");
     try {
-      await login({ email, password });
+      if (isSignup) {
+        await signup({ email, password });
+      } else {
+        await login({ email, password });
+      }
+      //await login({ email, password });
       navigate("/", { replace: true });
-    } catch (error) {
-      setErr(error.message || "Login Failed");
+    } catch (errorM) {
+      setErr(errorM.message || "Login Failed");
     }
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <h2>Sign in</h2>
+        <h2>{isSignup ? "Create Account" : "Sign-In"}</h2>
         <label>
           <span>Email</span>
           <input
@@ -43,10 +49,15 @@ export default function Login() {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="asdf"
+            placeholder="password123"
           />
         </label>
-        <button type="submit">Submit</button>
+        <button type="submit">{isSignup ? "Sign up" : "Login"}</button>
+
+        <p>
+          {isSignup ? "Already have an account" : "Don't have an account?"}{" "}
+          <button type="button"> {isSignup ? "Login" : "Sign Up"}</button>
+        </p>
       </form>
     </div>
   );
